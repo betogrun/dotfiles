5a74496c90fd1f473e049e6413315f51746f0d42
Alberto Rocha <betogrun@gmail.com>
2023-11-11 20:57:00 -0300

    Update asdf install

--- windows11/wsl2/ubuntu22.04/bootstrap.sh
+++ windows11/wsl2/ubuntu22.04/bootstrap.sh
@@ -52,18 +52,23 @@ echo "Installing Docker Compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# Install asdf
+echo "Installing asdf..."
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
+
 # Clone dotfiles repository
 echo "Cloning dotfiles..."
 git clone https://github.com/betogrun/dotfiles ~/dotfiles
 
+# Path for specific dotfiles
+dotfiles_path="$HOME/dotfiles/windows11/wsl2/ubuntu22.04"
+
 # Copy Git configuration from dotfiles
 echo "Configuring Git from dotfiles..."
 cp $dotfiles_path/.gitconfig ~/
-cp -r $dotfiles_path/.git_template ~/.git_template
 cp $dotfiles_path/.gitignore_global ~/
-
-# Path for specific dotfiles
-dotfiles_path="$HOME/dotfiles/windows11/wsl2/ubuntu22.04"
+cp -r $dotfiles_path/.git_template ~/.git_template
+chmod -R +x ~/.git_template/hooks
 
 # Install Oh My Zsh without user interaction
 echo "Installing Oh My Zsh..."
@@ -81,28 +86,13 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 # Copy Zsh, Powerlevel10k configuration from dotfiles
 echo "Configuring Zsh and Powerlevel10k from dotfiles..."
 rm ~/.zshrc
-# cp $dotfiles_path/.zshrc ~/
-cp "$HOME/test/.zshrc" ~/
+cp $dotfiles_path/.zshrc ~/
 cp $dotfiles_path/.p10k.zsh ~/
 
-# Add Powerlevel10k theme source to .zshrc
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-
 # Install Tmux Plugin Manager (TPM) and copy .tmux.conf from dotfiles
 echo "Installing Tmux Plugin Manager and configuring .tmux.conf..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp $dotfiles_path/.tmux.conf ~/
-
-# Install Tmux Plugin Manager (TPM) and copy .tmux.conf from dotfiles
-echo "Installing Tmux Plugin Manager and configuring .tmux.conf..."
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp $dotfiles_path/.tmux.conf ~/
-
-# Install asdf
-echo "Installing asdf..."
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
-echo '. $HOME/.asdf/asdf.sh' >> ~/.zshrc
-echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
 
 # Post-installation instructions
 echo "Post-installation steps:"
